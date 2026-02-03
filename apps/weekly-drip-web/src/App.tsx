1428e5c2685c73e9e05229a2923f6fec62ee21da
Andreas Stratmann <hello.andreas@protonmail.com>
2026-02-03 18:34:16 +0100

    chore(root): Clean up, add journal, init workspaces

--- apps/weekly-drip-web/src/App.tsx
+++ apps/weekly-drip-web/src/App.tsx
@@ -3,8 +3,8 @@
  * The main root component of the Weekly Drip application.
  * Contains the global layout and theme providers.
  */
-import AuditPage from "./audit/AuditPage";
 import "./App.css";
+import { setState, useStore } from "./store";
 
 /**
  * The primary application container.
@@ -13,11 +13,23 @@ import "./App.css";
  * In here the question becomes what is ultimately the most stable layout of the app.
  */
 export default function App() {
+  const title = useStore((s) => s.test);
+
   return (
-    <>
-      <h1>Weekly Drip</h1>
-      <nav></nav>
-      <AuditPage />
-    </>
+    <main>
+      <h1>Weekly Drip Calculator</h1>
+      <h2>{title}</h2>
+      <nav>
+        <button onClick={() => setState({ test: "hallo" })}>Add Item</button>
+        <button>List/Groups</button>
+      </nav>
+      <section className="results">
+        <li>Cost or Salary Item</li>
+        <li>Cost or Salary Item</li>
+        <li>Cost or Salary Item</li>
+        <li>Cost or Salary Item</li>
+        <li>Cost or Salary Item</li>
+      </section>
+    </main>
   );
 }
--- apps/weekly-drip-web/src/store.ts
+++ apps/weekly-drip-web/src/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useStore = create(() => ({
-  test: "Moin!",
+  test: "Ciao!",
 }));
 
 export const setState = useStore.setState;
