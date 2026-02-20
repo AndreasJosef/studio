cc4e87a31d350e5027bba3e34eee0db3fb482023
Andreas Stratmann <hello.andreas@protonmail.com>
2026-02-20 13:11:04 +0100

    feat(api): Implement token authentication middleware

---
+++ api/src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from 'express';
+import { jwtService } from '../services/jwt.service.ts';
+import { fail } from '@jobchaser/shared';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+/* eslint-enable @typescript-eslint/no-namespace */
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.auth_token;
+  if (!token) return res.status(401).json(fail('No access token provided!'));
+
+  const result = jwtService.validateToken(token);
+  if (!result.ok) return res.status(401).json(result);
+
+  req.userId = result.value.sub;
+
+  next();
+};
