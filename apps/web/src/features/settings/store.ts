597d4920d1c62f9995b1a4c8518b76cf94231408
Andreas Stratmann <hello.andreas@protonmail.com>
2026-03-19 16:50:32 +0100

    feat(web): Add settings store

---
+++ apps/web/src/features/settings/store.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type Theme = 'dark' | 'light' | 'system';
+
+interface SetttingsState {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+}
+
+export const useSettingsStore = create<SetttingsState>()(
+  persist(
+    (set) => ({
+      theme: 'dark',
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: 'jc-user-settings',
+    }
+  )
+);
